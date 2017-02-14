61ad1b9b242ef80a9e05fc7863484ed1b832784a
bczoch <bczoch@129-79-232-191.dhcp-bl.indiana.edu>
2017-02-14 16:07:58 -0500

    added disease page

---
+++ src/js_src/containers/disease/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Example extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Disease</h1>
+      </div>
+    );
+  }
+}
+
+export default Example;
--- src/js_src/routes.js
+++ src/js_src/routes.js
@@ -7,6 +7,7 @@ import Home from './containers/home';
 import Layout from './containers/layout';
 import Search from './containers/search';
 import GenePage from './containers/genePage';
+import Disease from './containers/disease';
 
 export default (
   <Route component={Layout} path='/'>
@@ -15,5 +16,6 @@ export default (
     <Route component={Help} path='help' />
     <Route component={Search} path='search' />
     <Route component={GenePage} path='gene/:geneId' />
+    <Route component={Disease} path='disease' />
   </Route>
 );
